79981edac17e5f5ff430961a5f38d82fad70247e
Niko Rainto <nikordn@gmail.com>
2024-06-28 22:34:28 +0300

    try changing to use pointers for Radiolib variables

--- src/main.cpp
+++ src/main.cpp
@@ -3,76 +3,44 @@
 #include <Adafruit_BME280.h>
 #include "config.h"
 
-// -- This is not working. Stuck in Reset_Handler: / Infinite_Loop:
 Adafruit_BME280 bme;
 
-float temperature, pressure, altitude, humidity;
+SX1262 *radio = nullptr;
+LoRaWANNode *node = nullptr;
 
 void setup()
 {
   Wire.begin();
-  bme.begin(0x76);
 
-  radio.begin();
-  radio.setRfSwitchPins(PA3, PA2);
+  if (!bme.begin(0x76))
+  {
+    while (1)
+      ;
+  }
 
-  node.beginOTAA(joinEUI, devEUI, nwkKey, appKey);
-  int16_t state = node.activateOTAA();
+  radio = new SX1262(new Module(PA4, PA0, PC4, PC5));
+
+  node = new LoRaWANNode(radio, &Region, subBand);
+
+  radio->begin();
+  radio->setRfSwitchPins(PA3, PA2);
+
+  node->beginOTAA(joinEUI, devEUI, nwkKey, appKey);
+  node->activateOTAA();
 }
 
 void loop()
 {
-  temperature = bme.readTemperature();
-  pressure = bme.readPressure() / 100.0F;
-  humidity = bme.readHumidity();
+  float temperature = bme.readTemperature();
+  float pressure = bme.readPressure() / 100.0F;
+  float humidity = bme.readHumidity();
 
   delay(2000);
 }
 
-// -- This is able to show values from BME280
-/*
-Adafruit_BME280 bme;
-
-float temperature, pressure, altitude, humidity;
-
-void setup()
+// Clean up dynamically allocated memory
+void cleanup()
 {
-  Wire.begin();
-  bme.begin(0x76);
-
-  radio.begin();
-  radio.setRfSwitchPins(PA3, PA2);
-
-  node.beginOTAA(joinEUI, devEUI, nwkKey, appKey);
+  delete node;
+  delete radio;
 }
-
-void loop()
-{
-  temperature = bme.readTemperature();
-  pressure = bme.readPressure() / 100.0F;
-  humidity = bme.readHumidity();
-
-  delay(2000);
-}
-*/
-
-// -- This is able to connect to LoRaWAN
-/*
-Adafruit_BME280 bme;
-
-float temperature, pressure, altitude, humidity;
-
-void setup()
-{
-  radio.begin();
-  radio.setRfSwitchPins(PA3, PA2);
-
-  node.beginOTAA(joinEUI, devEUI, nwkKey, appKey);
-  int16_t state = node.activateOTAA();
-}
-
-void loop()
-{
-  delay(2000);
-}
-*/
